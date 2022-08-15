cfb556e08f626c5efcb698240c95f2e7244bc52a
MarwaHany <30442426+MarwaHany@users.noreply.github.com>
2022-08-16 01:05:32 +0200

    Time: 177 ms (65.52%), Space: 14.6 MB (41.79%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -4,7 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        for i in range(len(nums)):
-            if sum(nums[:i]) == sum(nums[i+1:]):
-                return i
+        # for i in range(len(nums)):
+        #     if sum(nums[:i]) == sum(nums[i+1:]):
+        #         return i
+        # return -1
+        # Optimized solution
+        nums_sum = sum(nums)
+        left_handside_sum = 0
+        
+        for index, num in enumerate(nums):
+            if num+2*left_handside_sum == nums_sum:
+                return index
+            else:
+                left_handside_sum += num
         return -1
