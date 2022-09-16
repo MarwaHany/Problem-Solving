4c8f6d476598ebbee579f79c5e8ee1c258077d91
MarwaHany <30442426+MarwaHany@users.noreply.github.com>
2022-09-17 00:54:46 +0200

    Time: 54 ms (40.25%), Space: 13.8 MB (53.74%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -1,21 +1,6 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
-        temp = num
-        digits = []
-        while(temp):
-            digits.append(temp%10)
-            temp = temp//10
-        digits = digits[::-1]
-        try:
-            index = digits.index(6)
-        except:
-            return num
-        digits[index] = 9
-        res = 0
-        digits = digits[::-1]
-        for i in range(len(digits)):
-            res += digits[i]*10**i
-        return res
+        return int(str(num).replace("6","9",1))
             
             
         
